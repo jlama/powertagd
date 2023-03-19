405405e7069dd23833c067cf54313d5d0905cc62
John Lama <jlama@mistyos.org>
2023-03-19 15:53:42 +0100

    powertagd: add basic InfluxDB support

--- src/powertagd.c
+++ src/powertagd.c
@@ -1,10 +1,17 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
 #include <assert.h>
 #include <err.h>
+#include <errno.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "ash.h"
 #include "log.h"
@@ -12,6 +19,37 @@
 #include "zcl.h"
 #include "crypto/aes.h"
 #include "powertag.h"
+
+enum {
+	OUTPUT_FILE     = 0,
+	OUTPUT_INFLUXDB = 1,
+};
+
+static int output_type = OUTPUT_FILE;
+static FILE *output_file = NULL;
+
+static struct {
+	const char *url;
+	const char *org;
+	const char *bucket;
+	const char *token;
+	char hostname[128];
+	struct sockaddr_in addr;
+} influx_opts;
+
+/*
+ * InfluxDB metrics are written in batch every 30s.
+ * We assume the buffer is large enough to store all PowerTag reports for
+ * this time frame.
+ */
+#define INFLUXDB_BATCH_INTERVAL 30
+static struct {
+	pthread_t tid;
+	pthread_mutex_t lock; // To synchronize access to the following fields.
+	char data[128*1024];
+	size_t size;          // Total record size in data.
+	unsigned int nrec;    // Number of records in data.
+} influx_ctx;
 
 
 #ifdef ENABLE_MQTT
@@ -85,6 +123,136 @@ static void mqtt_close(void)
 	mosquitto_lib_cleanup();
 }
 #endif /* ENABLE_MQTT */
+
+
+static void *influx_thread(void *arg)
+{
+#define MAX_RETRY 3
+	int retries = 0;
+	char reply[512];
+	struct timespec ts_last, ts_now;
+
+	clock_gettime(CLOCK_MONOTONIC, &ts_last);
+
+	while (1) {
+		sleep(3);
+		clock_gettime(CLOCK_MONOTONIC, &ts_now);
+		int elapsed_ms = timespec_diff(&ts_last, &ts_now);
+		if (elapsed_ms < (INFLUXDB_BATCH_INTERVAL*1000))
+			continue;
+
+		ts_last = ts_now;
+		pthread_mutex_lock(&influx_ctx.lock);
+
+		if (influx_ctx.size == 0) {
+			pthread_mutex_unlock(&influx_ctx.lock);
+			continue;
+		}
+
+		LOG_DBG("influxdb: connecting to %s...", influx_opts.hostname);
+		int fd = socket(AF_INET, SOCK_STREAM, 0);
+		if (fd < 0)
+			LOG_FATAL("influxdb: cannot create socket: %s", strerror(errno));
+		if (connect(fd, (struct sockaddr *)&influx_opts.addr, sizeof(influx_opts.addr)) < 0) {
+			if (retries >= MAX_RETRY)
+				LOG_FATAL("influxdb: could not connect after %d retries, aborting", retries);
+
+			LOG_ERR("influxdb: failed to connect: %s", strerror(errno));
+			retries++;
+			pthread_mutex_unlock(&influx_ctx.lock);
+			continue;
+		}
+		retries = 0;
+
+		int res = dprintf(fd,
+		    "POST /api/v2/write?org=%s&bucket=%s&precision=s HTTP/1.1\r\n"
+		    "Host: %s\r\n"
+		    "Authorization: Token %s\r\n"
+		    "Content-Type: text/plain; charset=utf-8\r\n"
+		    "Accept: application/json\r\n"
+		    "Content-Length: %zu\r\n\r\n",
+		    influx_opts.org, influx_opts.bucket,
+		    influx_opts.hostname,
+		    influx_opts.token,
+		    influx_ctx.size);
+		if (res < 0)
+			LOG_FATAL("influxdb: write: %s", strerror(errno));
+
+		char *buf = influx_ctx.data;
+		size_t total = influx_ctx.size;
+		size_t sent = 0;
+		do {
+			ssize_t wr = write(fd, buf+sent, total-sent);
+			if (wr < 0)
+				LOG_FATAL("influxdb: write: %s", strerror(errno));
+			if (wr == 0)
+				break;
+			sent += wr;
+		} while (sent < total);
+
+		// Reset data and release lock
+		unsigned int nrec = influx_ctx.nrec;
+		influx_ctx.size = 0;
+		influx_ctx.nrec = 0;
+		pthread_mutex_unlock(&influx_ctx.lock);
+
+		// Read reply
+		total = sizeof(reply) - 1;
+		size_t received = 0;
+		do {
+			ssize_t rd = read(fd, reply+received, total-received);
+			if (rd < 0)
+				LOG_FATAL("influxdb: read: %s", strerror(errno));
+			if (rd >= 20)
+				break;
+			received += rd;
+		} while (received < total);
+
+		static char *http_ok = "HTTP/1.1 204";
+		if (strncmp(reply, http_ok, strlen(http_ok)) != 0) {
+			char *nl = strchr(reply, '\r');
+			if (nl != NULL)
+				LOG_ERR("influxdb: server error: %.*s", (int)(nl-reply), reply);
+			else
+				LOG_ERR("influxdb: unknown server error");
+		} else {
+			LOG_DBG("influxdb: wrote %u records", nrec);
+		}
+
+		close(fd);
+	}
+
+	return NULL;
+}
+
+static void write_influxdb(const char *fmt, va_list ap)
+{
+	pthread_mutex_lock(&influx_ctx.lock);
+
+	char *buf = influx_ctx.data + influx_ctx.size;
+	int res = vsprintf(buf, fmt, ap);
+	assert(res > 0);
+	influx_ctx.size += res;
+	influx_ctx.nrec++;
+
+	pthread_mutex_unlock(&influx_ctx.lock);
+}
+
+static void write_report(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+
+	switch (output_type) {
+	case OUTPUT_FILE:
+		vfprintf(output_file, fmt, ap);
+		break;
+	case OUTPUT_INFLUXDB:
+		write_influxdb(fmt, ap);
+		break;
+	}
+	va_end(ap);
+}
 
 
 static bool handle_rx_gp_frame(const EzspFrame *frame)
@@ -462,7 +630,7 @@ static void gpf_process_mfr_specific_reporting(const GpFrame *f)
 	if (str[len-1] == ',')
 		str[len-1] = '\0';
 
-	printf("powertag,id=0x%08x %s %lu\n", srcid, str, timestamp);
+	write_report("powertag,id=0x%08x %s %lu\n", srcid, str, timestamp);
 }
 
 static void gpf_process_mfr_multi_cluster_reporting(const GpFrame *f)
@@ -487,11 +655,50 @@ static bool gp_callback_handler(const GpFrame *f)
 	return true;
 }
 
+static int parse_output_arg(const char *arg)
+{
+	if (strcmp(arg, "influxdb") == 0)
+		return OUTPUT_INFLUXDB;
+
+	if (strcmp(arg, "-") == 0 || strcmp(arg, "stdout") == 0) {
+		output_file = stdout;
+		return OUTPUT_FILE;
+	}
+
+	output_file = fopen(arg, "a");
+	if (output_file == NULL)
+		err(1, "%s", arg);
+
+	return OUTPUT_FILE;
+}
+
 static void usage(void)
 {
 	printf("Usage: powertagd [-qv] [-d device]\n");
 	exit(1);
 }
+
+enum {
+	INFLUX_URL = 1000,
+	INFLUX_ORG,
+	INFLUX_BUCKET,
+	INFLUX_TOKEN,
+};
+
+static struct option long_opts[] = {
+	{"dev",      required_argument, NULL, 'd'},
+	{"output",   required_argument, NULL, 'o'},
+	{"quiet",    no_argument, NULL, 'q'},
+	{"verbose",  no_argument, NULL, 'q'},
+
+	// InfluxDB options
+	{"url",    required_argument, NULL, INFLUX_URL},
+	{"org",    required_argument, NULL, INFLUX_ORG},
+	{"bucket", required_argument, NULL, INFLUX_BUCKET},
+	{"token",  required_argument, NULL, INFLUX_TOKEN},
+
+	{NULL, 0, NULL, 0}
+};
 
 int main(int argc, char **argv)
 {
@@ -499,7 +706,7 @@ int main(int argc, char **argv)
 	int verbose = 1;
 
 	int ch;
-	while ((ch = getopt(argc, argv, "d:qv")) != -1) {
+	while ((ch = getopt_long(argc, argv, "d:o:qv", long_opts, NULL)) != -1) {
 		switch (ch) {
 		case 'q':
 			verbose = 0;
@@ -510,6 +717,23 @@ int main(int argc, char **argv)
 		case 'd':
 			serialdev = optarg;
 			break;
+		case 'o':
+			output_type = parse_output_arg(optarg);
+			break;
+
+		// InfluxDB options
+		case INFLUX_URL:
+			influx_opts.url = optarg;
+			break;
+		case INFLUX_ORG:
+			influx_opts.org = optarg;
+			break;
+		case INFLUX_BUCKET:
+			influx_opts.bucket = optarg;
+			break;
+		case INFLUX_TOKEN:
+			influx_opts.token = optarg;
+			break;
 		default:
 			usage();
 			// NOTREACHED
@@ -517,11 +741,6 @@ int main(int argc, char **argv)
 	}
 	argv += optind;
 	argc -= optind;
-
-	if (serialdev == NULL) {
-		warnx("no serial device specified (-d)");
-		usage();
-	}
 
 	log_init();
 	switch (verbose) {
@@ -538,6 +757,65 @@ int main(int argc, char **argv)
 
 	// Line-buffered stdout
 	setlinebuf(stdout);
+
+	if (output_file == NULL)
+		output_file = stdout;
+
+	if (serialdev == NULL) {
+		warnx("no serial device specified (-d)");
+		usage();
+	}
+	if (output_type == OUTPUT_INFLUXDB) {
+		if (!influx_opts.url)
+			errx(1, "influxdb output requires --url option");
+		if (!influx_opts.org)
+			errx(1, "influxdb output requires --org option");
+		if (!influx_opts.bucket)
+			errx(1, "influxdb output requires --bucket option");
+		if (!influx_opts.token)
+			errx(1, "influxdb output requires --token option");
+
+		// Parse URL. It should look like http://host:port
+		const char *url = influx_opts.url;
+		char *hostname = influx_opts.hostname;
+		int port = 8086; // Default InfluxDB port.
+
+		const char *protocol = "http://";
+		if (strncmp(url, protocol, strlen(protocol)) != 0)
+			errx(1, "invalid influxdb url '%s'", url);
+
+		// Skip prefix
+		url += strlen(protocol);
+		char *colon = strrchr(url, ':');
+		if (colon) {
+			memcpy(hostname, url, colon-url);
+			hostname[colon-url] = '\0';
+			port = atoi(colon + 1);
+		} else {
+			memcpy(hostname, url, strlen(url));
+			hostname[strlen(url)] = '\0';
+		}
+
+		struct hostent *he = gethostbyname(hostname);
+		if (he == NULL)
+			err(1, "gethostbyname");
+
+		memset(&influx_opts.addr, 0, sizeof(influx_opts.addr));
+		influx_opts.addr.sin_family = AF_INET;
+		influx_opts.addr.sin_port = htons(port);
+		memcpy(&influx_opts.addr.sin_addr.s_addr, he->h_addr, he->h_length);
+
+		char ip[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(influx_opts.addr.sin_addr), ip, INET_ADDRSTRLEN);
+		LOG_INFO("InfluxDB host: %s:%i", ip, port);
+
+		int r = pthread_mutex_init(&influx_ctx.lock, NULL);
+		if (r != 0)
+			err(1, "pthread_mutex_init");
+		r = pthread_create(&influx_ctx.tid, NULL, &influx_thread, NULL);
+		if (r != 0)
+			err(1, "pthread_create");
+	}
 
 	serial_open(serialdev, BAUDRATE);
 	ash_init(serial_read, serial_write);
